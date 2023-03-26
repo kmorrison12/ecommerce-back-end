4dfbe73e030db37e52c76a6e73a25fca6cdbd7b8
kmorrison1 <morrison.katie93@gmail.com>
2023-03-26 14:43:27 -0400

    updated models

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
+const { INTEGER } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    catergory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
